f29eb2c835fd70398239884ec7d540de9d910ff2
jacklee5 <jack200336@gmail.com>
2019-09-11 13:38:02 -0400

    work

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
 
-app.get('/', (req, res) => res.send('Pee!'))
+app.get('/', (req, res) => res.send('Pee!'));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+const port = process.env.PORT || 1337;
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
