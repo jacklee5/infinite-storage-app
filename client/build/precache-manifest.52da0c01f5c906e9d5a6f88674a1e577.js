c6883b9c94e5e8d9e7a2434aba971285c319938a
jacklee5 <jack200336@gmail.com>
2019-12-04 10:11:40 -0500

    go

---
+++ client/build/precache-manifest.52da0c01f5c906e9d5a6f88674a1e577.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4a9faec856a6687ffc0c829ff187cc8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "be0ccc6d35b7ffe6b12f",
+    "url": "/static/css/main.dbc9a29c.chunk.css"
+  },
+  {
+    "revision": "3ff8ff475c45f9adcb2b",
+    "url": "/static/js/2.9045cfd3.chunk.js"
+  },
+  {
+    "revision": "be0ccc6d35b7ffe6b12f",
+    "url": "/static/js/main.1d9ab61f.chunk.js"
+  },
+  {
+    "revision": "b3938888db487c1703c6",
+    "url": "/static/js/runtime~main.a7ca617d.js"
+  },
+  {
+    "revision": "4d22136b9d11ef71f11a974227636fa0",
+    "url": "/static/media/BerdBoxLogo.4d22136b.png"
+  },
+  {
+    "revision": "cc300e12ebe0a495ba446a798b70d83c",
+    "url": "/static/media/Folder.cc300e12.png"
+  },
+  {
+    "revision": "422dc8f0eee4f03973eb08c2a3b058bb",
+    "url": "/static/media/Loading.422dc8f0.gif"
+  },
+  {
+    "revision": "02c5b76615a93752988620a3fe307235",
+    "url": "/static/media/Movie.02c5b766.gif"
+  },
+  {
+    "revision": "3a87b6d52e2ed905662685b86ac324b8",
+    "url": "/static/media/Picture.3a87b6d5.png"
+  },
+  {
+    "revision": "1494db0b870eb6e70f1a6ca9db52ff86",
+    "url": "/static/media/sad.1494db0b.png"
+  },
+  {
+    "revision": "c9d38e513bad52499b373f09424e4ff1",
+    "url": "/static/media/sound.c9d38e51.png"
+  },
+  {
+    "revision": "56fc3700b1e9e370a00804c03b50ec8c",
+    "url": "/static/media/txt.56fc3700.png"
+  }
+]);
